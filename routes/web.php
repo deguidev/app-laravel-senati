18b8ff8b161244aa38b559f68d535e17756b6907
deguidev <dlp.diegolipa@gmail.com>
2025-11-20 13:13:49 -0500

    primer commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Laravel\Fortify\Features;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canRegister' => Features::enabled(Features::registration()),
+    ]);
+})->name('home');
+
+Route::get('dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('hola-mundo', function () {
+    return Inertia::render('HolaMundo');
+})->middleware(['auth', 'verified'])->name('hola-mundo');
+
+
+
+
+
+
+
+
+
+
+
+require __DIR__.'/settings.php';
